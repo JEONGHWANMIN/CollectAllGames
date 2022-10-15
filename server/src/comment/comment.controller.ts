fea9756d91a6114326902ff0334e1c512c5603f3
JEONGHWANMIN <mimi0j0j@naver.com>
2022-10-16 04:22:45 +0900

    Feat : create comment

--- server/src/comment/comment.controller.ts
+++ server/src/comment/comment.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { JwtPayload } from 'src/auth/types';
+import { GetCurrentUser } from 'src/common/decorators';
+import { CommentService } from './comment.service';
+import { CommentDto } from './dto';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+  constructor(private commentService: CommentService) {}
+
+  @Post(':postId')
+  create(
+    @Param('postId', ParseIntPipe) postId: number,
+    @GetCurrentUser() user: JwtPayload,
+    @Body() dto: CommentDto,
+  ) {
+    return this.commentService.create(postId, user, dto);
+  }
+}
--- server/src/comment/comment.service.ts
+++ server/src/comment/comment.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { JwtPayload } from 'src/auth/types';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CommentDto } from './dto';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(postId: number, user: JwtPayload, dto: CommentDto) {
+    await this.prisma.comment.create({
+      data: {
+        content: dto.content,
+        postId,
+        userId: user.userId,
+      },
+    });
+    return 'comment created';
+  }
+}
