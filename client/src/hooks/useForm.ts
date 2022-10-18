69ea2ea4fa606e589b5c1bd8db51cb46936f5a4a
JEONGHWANMIN <mimi0j0j@naver.com>
2022-10-19 04:52:05 +0900

    Feat : add useForm

--- client/src/hooks/useForm.ts
+++ client/src/hooks/useForm.ts
@@ -1,27 +1,85 @@
 import React, { useState } from "react";
 
-interface ReturnsType<T> {
+interface ReturnsType<T, K> {
   formData: T;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleSubmit: (e: React.FormEvent<HTMLDivElement>) => void;
+  handleSubmit: (
+    e: React.FormEvent<HTMLDivElement>,
+    callback: () => void
+  ) => void;
+  errors: K;
+}
+interface FormType {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  username: string;
 }
 
-function useForm<T>(initialState: T): ReturnsType<T> {
+interface validateType {
+  emailValidate: (email: string) => boolean;
+  passwordValidate: (password: string) => boolean;
+  passwordConfirmValidate: (
+    password: string,
+    passwordConfirm: string
+  ) => boolean;
+  userNameValidate: (username: string) => boolean;
+}
+
+function useForm<T extends FormType, K>(
+  initialState: T,
+  validation: validateType
+): ReturnsType<T, K> {
   const [formData, setFormData] = useState(initialState);
+  const [errors, setErrors] = useState({} as K);
   const [, setIsLoading] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.name);
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
+
+    if (e.target.name === "email") {
+      setErrors({
+        ...errors,
+        email: validation.emailValidate(e.target.value),
+      });
+    }
+
+    if (e.target.name === "password") {
+      setErrors({
+        ...errors,
+        password: validation.passwordValidate(e.target.value),
+      });
+    }
+
+    if (e.target.name === "passwordConfirm") {
+      setErrors({
+        ...errors,
+        passwordConfirm: validation.passwordConfirmValidate(
+          formData.password,
+          e.target.value
+        ),
+      });
+    }
+
+    if (e.target.name === "username") {
+      setErrors({
+        ...errors,
+        username: validation.userNameValidate(e.target.value),
+      });
+    }
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLDivElement>) => {
+  const handleSubmit = (
+    e: React.FormEvent<HTMLDivElement>,
+    callback: () => void
+  ) => {
     e.preventDefault();
     setIsLoading(true);
     console.log(formData);
+    callback();
     setIsLoading(false);
   };
 
@@ -29,6 +87,7 @@ function useForm<T>(initialState: T): ReturnsType<T> {
     formData,
     handleChange,
     handleSubmit,
+    errors,
   };
 }
 
