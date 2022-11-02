6208d43c140b14d160a29411b7f55e12db9aebaf
JEONGHWANMIN <mimi0j0j@naver.com>
2022-11-02 21:03:59 +0900

    Feat : add react-query devtools

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,10 +7,9 @@ import { router } from "./pages/router";
 import { UserProvider } from "./context/userContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./style/fonts.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 const queryClient = new QueryClient();
 
 root.render(
@@ -20,6 +19,7 @@ root.render(
         <Global styles={reset} />
         <RouterProvider router={router} />
       </UserProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
