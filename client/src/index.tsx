1d4fd6e872d725862e49d284f1ac08d9bb6eca4a
JEONGHWANMIN <mimi0j0j@naver.com>
2022-11-11 10:52:06 +0900

    Feat : add DialogProvider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,6 +8,7 @@ import { UserProvider } from "./context/userContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./style/fonts.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { DialogProvider } from "./context/dialogContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 const queryClient = new QueryClient();
@@ -16,8 +17,10 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <UserProvider>
-        <Global styles={reset} />
-        <RouterProvider router={router} />
+        <DialogProvider>
+          <Global styles={reset} />
+          <RouterProvider router={router} />
+        </DialogProvider>
       </UserProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
